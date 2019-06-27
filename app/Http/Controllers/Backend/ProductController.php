8bc3f550223b496992a4239a0bb79bddcd3a3327
dathalongbay <dathalongbay@gmail.com>
2019-06-27 20:45:56 +0700

    1

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -81,7 +81,26 @@ class ProductController extends Controller
     }
 
 
-    public function update() {
+    public function update($id, Request $request) {
+
+        $request->validate([
+            'product_name' => 'required|max:255|min:5',
+            'product_slug' => 'required',
+            'product_description' => 'required',
+        ]);
+
+        $inputs = $request->all();
+
+        $productModel = ProductModel::find($id);
+
+        $productModel->product_name = isset($inputs["product_name"]) ? $inputs["product_name"] : "";
+        $productModel->product_slug = isset($inputs["product_slug"]) ? $inputs["product_slug"] : "";
+        $productModel->product_images = isset($inputs["product_images"]) ? $inputs["product_images"] : "";
+        $productModel->product_description = isset($inputs["product_description"]) ? $inputs["product_description"] : "";
+
+        $productModel->save();
+
+        return redirect("/admin/products");
 
     }
 

bd830df46841205932db7e047529be1e09ef4762
dathalongbay <dathalongbay@gmail.com>
2019-07-09 19:06:00 +0700

    1

--- public/abstract/mysql.php
+++ public/abstract/mysql.php
@@ -1,6 +1,19 @@
 <?php
 
-class ConnectMySQL extends ConnectDatabase{
+/**
+ * Class ConnectMySQL
+ * KHi 1 class muốn thực thi 1 interface
+ * thì class đó sẽ sử dụng từ khoá implements tên của interface
+ * 1 class thì có thể implements nhiều interface
+ * Nhưng 1 class thì chỉ có thể kế thừa từ 1 class abstract mà thôi
+ *
+ *
+ * Abstract : cái class đó là gì ?
+ * Interface : class đó làm được những gì ?
+ *
+ *
+ */
+class ConnectMySQL extends ConnectDatabase implements Excel, CSV {
 
 
     /**
@@ -11,6 +24,26 @@ class ConnectMySQL extends ConnectDatabase{
     public function connectDatabase()
     {
         $connect = mysqli_connect("localhost", "root", "", "test");
+    }
+
+
+    public function importExcel(){
+        echo "<br>" . __METHOD__;
+    }
+
+
+    public function exportExcel(){
+        echo "<br>" . __METHOD__;
+    }
+
+
+    public function importCSV(){
+        echo "<br>" . __METHOD__;
+    }
+
+
+    public function exportCSV(){
+        echo "<br>" . __METHOD__;
     }
 
 }
